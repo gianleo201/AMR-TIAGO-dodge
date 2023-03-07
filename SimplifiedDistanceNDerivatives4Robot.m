eb663dd87b014dd14b8f110cef0769c134e99657
DavideMugi <67126770+DavideMugi@users.noreply.github.com>
2023-03-07 13:21:01 +0100

    Update SimplifiedDistanceNDerivatives4Robot.m

--- SimplifiedDistanceNDerivatives4Robot.m
+++ SimplifiedDistanceNDerivatives4Robot.m
@@ -1,10 +1,10 @@
-function [d, dot_d, ddot_d]=SimplifiedDistanceNDerivatives4Robot(P_obs,r_obs)
+function [d, dot_d, ddot_d, d0]=SimplifiedDistanceNDerivatives4Robot(P_obs,r_obs, x0)
 
 syms x [6 1] real; syms tau [1 3] real; syms t real;
 dx = TIAGO(t,x,tau,0,0);
 
 
-%coordinate is (x y) (row vector), x -->horizontal, y --> vertical
+%coordinate is (x; y) (column vector), x -->horizontal, y --> vertical
 
 
 hb = 0.193;
@@ -16,6 +16,7 @@ l3 = 0.34+0.25;
 
 P = sym('P', [2 1 3]);
 d = sym('d', [1, 3]);
+d0 = sym('d0', [1 3]);
 dot_d = sym('dot_d', [1, 3]);
 ddot_d = sym('ddot_d', [1, 3]);
 r = sym('r', [1 3]);
@@ -30,6 +31,8 @@ P(:, :, 3) = [x1 - rt1 + rt4 - l3/2*sin(x2 + x3) - l2*sin(x2); hb + ht + l3/2*co
 
 for i=1:3
     [d(i), dot_d(i), ddot_d(i)]=DistanceNDerivatives(P(:, :, i), r(i), P_obs, r_obs, x, dx);
+    d0(i)=subs(d(i), x, x0);
 end
+
 
 end
